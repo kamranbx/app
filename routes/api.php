4bee6c1c3de3c8def4c12bb70d1d208730eb4da2
kamranbx <kamranbaxshaliyev567@gmail.com>
2025-06-13 11:44:20 +0400

    minor fix

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 Route::post('refresh-token', [\App\Http\Controllers\AuthController::class, 'refreshToken']);
 
 Route::middleware('jwt')->group(function () {
